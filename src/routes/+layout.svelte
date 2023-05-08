6bec67b0e188d4eab2e1cb364324e954bd1c209e
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2023-05-08 23:49:55 +0900

    20230508

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Header from './common/Header.svelte';
+  import './styles.css';
+</script>
+
+<div>
+  <Header />
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    ふったー
+  </footer>
+</div>
+
+<style>
+
+</style>
---
+++ src/routes/common/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts"></script>
+
+<header id="global-header">
+  <div class="header-content">
+    男子新体操国際化プロジェクト
+  </div>
+</header>
+
+<style>
+  #global-header {
+    display:    block;
+    width:      100%;
+    height:     40px;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 150);
+    padding:    20px 0;
+  }
+
+  .header-content {
+    width:     1024px;
+    margin:    0 auto;
+    font-size: 24px;
+  }
+</style>
