334135ffd7f1174fbbbc5e0db5fed97b01548442
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2023-07-02 17:33:15 +0900

    fix design

--- src/routes/sp/common/form/ExecutionPointResultModal.svelte
+++ src/routes/sp/common/form/ExecutionPointResultModal.svelte
@@ -11,9 +11,9 @@
     getDeductionOfDroppedApparatus
   } from '../../../../ts/form/executionDeduct/Culculator';
 
-  let pointA:           number;
-  let pointB:           number;
-  let decisionPoints:   number;
+  let pointA:         number;
+  let pointB:         number;
+  let decisionPoints: number;
   export let show;
 
   executionDeduct.subscribe(data => {
@@ -216,14 +216,15 @@
       }
 
       &__title {
-        display:       inline-block;
-        padding:       0 24px;
-        font-size:     24px;
-        font-weight:   bold;
-        color:         white;
-        margin:        0 0 16px;
-        border-radius: 8px;
-        background:    #32538D;
+        display:        inline-block;
+        margin:         0;
+        padding:        0 24px;
+        font-size:      24px;
+        font-weight:    bold;
+        color:          white;
+        border-radius:  8px;
+        background:     #32538D;
+        vertical-align: middle;
       }
 
       &__format {
@@ -282,12 +283,16 @@
       vertical-align: middle;
 
       &__pull-down-button {
-        margin-bottom: 8px;
-        background:    white;
-        border-radius: 4px;
-        font-family:   YuGothic, sans-serif;
-
-        &:hover { cursor: pointer; }
+        display:        inline-block;
+        width:          100px;
+        height:         32px;
+        font-size:      14px;
+        font-weight:    bold;
+        background:     white;
+        border-radius:  4px;
+        border:         2px solid #555555;
+        font-family:    YuGothic, sans-serif;
+        vertical-align: middle;
       }
 
       &__item {
