877cb03ef3032c7a8c515bb66cdde985ecfaba3e
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2023-07-23 10:58:33 +0900

    commit 20230723

--- src/routes/pc/rules/Root.svelte
+++ src/routes/pc/rules/Root.svelte
@@ -45,8 +45,7 @@
   }
 </script>
 
-<article id="mrg-rules">
-
+<article id="mrg-rules" class="rule-book">
   <h1>{vvRuleBook.title}</h1>
   {#each vvRuleBook.chapter as chapter, chapterIndex}
     <h2>第{chapterIndex + 1}章 {chapter.title}</h2>
@@ -56,14 +55,18 @@
 
       {#each article.section as section, sectionIndex}
         {#if section.block.length}
-          {sectionIndex + 1} {section.title}
+          <h4>{sectionIndex + 1} {section.title}</h4>
           {#each section.block as block, blockIndex}
-            <h4>第{calculateIndexOfArticle(chapterIndex, articleIndex, sectionIndex, blockIndex)}条 {block.title}</h4>
-            <p>{@html block.element.replaceAll('\n', '<br>')}</p>
+            <div class="rule-book__record">
+              <h5>第{calculateIndexOfArticle(chapterIndex, articleIndex, sectionIndex, blockIndex)}条 {block.title}</h5>
+              <p class="rule-book__record-text">{@html block.element.replaceAll('\n', '<br>')}</p>
+            </div>
           {/each}
         {:else}
-          <h4>第{calculateIndexOfArticle(chapterIndex, articleIndex, sectionIndex)}条 {section.title}</h4>
-          <p>{@html section.content.replaceAll('\n', '<br>')}</p>
+          <div class="rule-book__record">
+            <h4 class="rule-book__record-title">第{calculateIndexOfArticle(chapterIndex, articleIndex, sectionIndex)}条 {section.title}</h4>
+            <p class="rule-book__record-text">{@html section.content.replaceAll('\n', '<br>')}</p>
+          </div>
         {/if}
       {/each}
     {/each}
@@ -77,5 +80,13 @@
   #mrg-rules {
     width:  1024px;
     margin: 0 auto;
+  }
+
+  .rule-book {
+    &__record {
+      margin-bottom: 48px;
+    }
+
+    &__record-text { margin: 0; }
   }
 </style>
