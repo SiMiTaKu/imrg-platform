7dadfd16ff728a11708ef00102d17a8c71b991b3
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-01-02 12:00:17 +0900

    fix rules page

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import PCRoot from "../../web/pc/rules/Root.svelte";
-  import SPRoot from "../../web/sp/rules/Root.svelte";
+  import RootPC from "../../web/rules/RootPC.svelte";
+  import RootSP from "../../web/rules/RootSP.svelte";
   import {
     getResponsiveDesign,
     designOfPC,
@@ -17,9 +17,9 @@
 <svelte:window bind:outerWidth={screenWidth} />
 
 {#if getResponsiveDesign(screenWidth) === designOfPC}
-  <PCRoot />
+  <RootPC />
 {:else if getResponsiveDesign(screenWidth) === designOfSP}
-  <SPRoot />
+  <RootSP />
 {/if}
 
 <style lang="scss">
