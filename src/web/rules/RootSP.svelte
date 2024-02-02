719986a185854438bfc55f9cc5e99d55679afbca
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-02-02 19:21:16 +0900

    SP側にBlockの画像を表示

--- src/web/rules/RootSP.svelte
+++ src/web/rules/RootSP.svelte
@@ -29,6 +29,17 @@
                 )}条 {block.title}
               </h5>
               <p class="rule-book__record-text">{block.element}</p>
+              {#if block.image}
+                {#each block.image as image}
+                  <div>
+                    <img
+                      src={`/image/rules/${image.fileName}`}
+                      alt={image.alt}
+                      width="100%"
+                    />
+                  </div>
+                {/each}
+              {/if}
             </div>
           {/each}
         {:else}
