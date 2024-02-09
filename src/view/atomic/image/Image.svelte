1567ad5bee50c2a86b544679dba4146750c39f3e
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-02-09 18:28:03 +0900

    image componentの作成

---
+++ src/view/atomic/image/Image.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+  import type { Image } from '../../../ts/common/image'
+</script>
+
+<script lang="ts">
+  import { toggleImageOpacityOnInterSection } from './_service/fade-in-image'
+  export let image: Image
+  export let width: string = 'auto'
+  export let height: string = 'auto'
+  export let isLazy: boolean = true // lazyloadのフラグ
+  /** 画像をフェードインさせるかどうか */
+  export let fadeIn: boolean = false
+
+  const fadeInImage = fadeIn
+    ? toggleImageOpacityOnInterSection
+    : () => {}
+</script>
+
+<img
+  {width}
+  {height}
+  src={image.url}
+  loading={isLazy ? 'lazy' : 'eager'}
+  alt={image.alt}
+  style:transition={fadeIn ? '1s' : 'none'}
+  style:opacity={fadeIn ? 0 : 1}
+  use:fadeInImage
+/>
