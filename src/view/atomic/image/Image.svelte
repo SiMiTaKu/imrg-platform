3bd4d3f1602743da1c8ecebc538e102ae4dc56d3
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-02-09 18:30:06 +0900

    微修正

--- src/view/atomic/image/Image.svelte
+++ src/view/atomic/image/Image.svelte
@@ -1,14 +1,13 @@
 <script lang="ts" context="module">
   import type { Image } from '../../../ts/common/image'
+  import { toggleImageOpacityOnInterSection } from './_service/fade-in-image'
 </script>
 
 <script lang="ts">
-  import { toggleImageOpacityOnInterSection } from './_service/fade-in-image'
   export let image: Image
   export let width: string = 'auto'
   export let height: string = 'auto'
-  export let isLazy: boolean = true // lazyloadのフラグ
-  /** 画像をフェードインさせるかどうか */
+  export let isLazy: boolean = true
   export let fadeIn: boolean = false
 
   const fadeInImage = fadeIn
