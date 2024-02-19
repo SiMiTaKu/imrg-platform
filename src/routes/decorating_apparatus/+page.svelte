8297d9e507d468b9fee0359a09baeff4869b1f52
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-02-19 23:21:24 +0900

    空ファイルのみ作成

---
+++ src/routes/decorating_apparatus/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Root from "../../view/page/decorating_apparatus/Root.svelte";
+</script>
+
+<svelte:head>
+  <title>手具装飾 | 男子新体操国際化プロジェクト</title>
+</svelte:head>
+
+<Root />
