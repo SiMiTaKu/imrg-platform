0b422548f1ed8ae59438ea7b899eddce5763178a
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-02-23 14:11:25 +0900

    base

--- src/view/page/decorating_apparatus/Root.svelte
+++ src/view/page/decorating_apparatus/Root.svelte
@@ -1,1 +1,11 @@
-// TODO: やることを列挙する
+<script lang="ts" context="module">
+  import MainVisual from "./_component/MainVisual.svelte";
+  import WorkList from "./_component/WorkList.svelte";
+  import Price from "./_component/Price.svelte";
+  import Contact from "./_component/Contact.svelte";
+</script>
+
+<MainVisual />
+<WorkList />
+<Price />
+<Contact />
