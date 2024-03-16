e7d1084d0366ec026e93e7500c6b3b6fd84d3036
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-03-16 18:03:53 +0900

    ラジオボタンのオプションを引っ越し

--- src/view/page/judge/Root.svelte
+++ src/view/page/judge/Root.svelte
@@ -1,5 +1,5 @@
 <script lang="ts" context="module">
-  import Radio from "./_component/RadioPC.svelte";
+  import Radio from "./_component/Radio.svelte";
   import ExecutionPointResultModalPC from "./_component/ExecutionPointResultModalPC.svelte";
   import ExecutionPointResultModalSP from "./_component/ExecutionPointResultModalSP.svelte";
   import {
@@ -11,10 +11,7 @@
 
 <script lang="ts">
   import { executionDeduct } from "./store/store";
-  import { deductionOptions } from "./_service/model";
   import { fly } from "svelte/transition";
-
-  const options = deductionOptions;
 
   let submitted: boolean = false;
   function submit() {
@@ -44,63 +41,54 @@
         <div>※あなたが感じた直感を信じて1〜10点満点で選択してください。</div>
         <h3 class="form-container__title">徒手の技術</h3>
         <Radio
-          {options}
           title={$executionDeduct.pointA.beautifulPose.title}
           bind:userSelected={$executionDeduct.pointA.beautifulPose.value}
           annotation={$executionDeduct.pointA.beautifulPose.annotation}
           uniqueId="beautifulPose"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.flexibility.title}
           bind:userSelected={$executionDeduct.pointA.flexibility.value}
           annotation={$executionDeduct.pointA.flexibility.annotation}
           uniqueId="flexibility"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.naturalMovement.title}
           bind:userSelected={$executionDeduct.pointA.naturalMovement.value}
           annotation={$executionDeduct.pointA.naturalMovement.annotation}
           uniqueId="naturalMovement"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.bendingWeight.title}
           bind:userSelected={$executionDeduct.pointA.bendingWeight.value}
           annotation={$executionDeduct.pointA.bendingWeight.annotation}
           uniqueId="bendingWeight"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.jumpingHeight.title}
           bind:userSelected={$executionDeduct.pointA.jumpingHeight.value}
           annotation={$executionDeduct.pointA.jumpingHeight.annotation}
           uniqueId="jumpingHeight"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.bodyControl.title}
           bind:userSelected={$executionDeduct.pointA.bodyControl.value}
           annotation={$executionDeduct.pointA.bodyControl.annotation}
           uniqueId="bodyControl"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.heelRaise.title}
           bind:userSelected={$executionDeduct.pointA.heelRaise.value}
           annotation={$executionDeduct.pointA.heelRaise.annotation}
           uniqueId="heelRaise"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.weaknessAndStrength.title}
           bind:userSelected={$executionDeduct.pointA.weaknessAndStrength.value}
           annotation={$executionDeduct.pointA.weaknessAndStrength.annotation}
           uniqueId="weaknessAndStrength"
         />
         <Radio
-          {options}
           title={$executionDeduct.pointA.connectMovement.title}
           bind:userSelected={$executionDeduct.pointA.connectMovement.value}
           annotation={$executionDeduct.pointA.connectMovement.annotation}
@@ -108,7 +96,6 @@
         />
         <h3 class="form-container__title">手具の技術</h3>
         <Radio
-          {options}
           title={$executionDeduct.pointA.apparatusControl.title}
           bind:userSelected={$executionDeduct.pointA.apparatusControl.value}
           annotation={$executionDeduct.pointA.apparatusControl.annotation}
@@ -116,7 +103,6 @@
         />
         <h3 class="form-container__title">音楽</h3>
         <Radio
-          {options}
           title={$executionDeduct.pointA.musicImage.title}
           bind:userSelected={$executionDeduct.pointA.musicImage.value}
           annotation={$executionDeduct.pointA.musicImage.annotation}
--- src/view/page/judge/_component/Radio.svelte
+++ src/view/page/judge/_component/Radio.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-  import type { RadioDeduction } from "../_service/model";
   export let title: string;
-  export let options: RadioDeduction[];
   export let userSelected: number;
   export let annotation: string;
   export let uniqueId: string;
+
+  interface RadioDeduction {
+    label: string;
+    value: number;
+  }
+
+  const OPTIONS: RadioDeduction[] = [
+    { label: "1", value: 1 },
+    { label: "2", value: 2 },
+    { label: "3", value: 3 },
+    { label: "4", value: 4 },
+    { label: "5", value: 5 },
+    { label: "6", value: 6 },
+    { label: "7", value: 7 },
+    { label: "8", value: 8 },
+    { label: "9", value: 9 },
+    { label: "10", value: 10 },
+  ];
 </script>
 
 <div class="radio-question">
@@ -16,7 +32,7 @@
     aria-checked="false"
     aria-labelledby="label-${uniqueId}"
   >
-    {#each options as option}
+    {#each OPTIONS as option}
       <input
         id={option.label + uniqueId}
         type="radio"
