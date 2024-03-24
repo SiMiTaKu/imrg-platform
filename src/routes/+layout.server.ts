d1b04d0853f4ab0a64be34bf1ffa7bd1feb82371
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-03-24 18:38:20 +0900

    微修正

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-
-type Output = App.PageData;
-
-export const load = (async ({ locals }) => {
-  /** @desc 現在amplifyでhooksが動作していない */
-  return { isMobile: locals.isMobile };
-}) satisfies LayoutServerLoad<Output>;
--- svelte.config.js
+++ svelte.config.js
@@ -18,11 +18,6 @@ const config = {
       precompress: false,
       strict:      true
     }),
-    files: {
-      hooks: {
-        server: 'src/server/hooks/hooks.server.ts',
-      }
-    }
   }
 };
 
