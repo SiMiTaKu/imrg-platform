80eec406e1301c5d0fc3fd05224087c86fea1604
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-05-19 21:54:29 +0900

    手具のEnumを作成

---
+++ src/view/page/judge/_model/apparatus.ts
@@ -0,0 +1,26 @@
+/** 手具のEnum **/
+const APPARATUS = {
+  /** スティック */
+  STICK: {
+    code: 1,
+    name: "スティック",
+  },
+  /** リング */
+  RING: {
+    code: 2,
+    name: "リング",
+  },
+  /** ロープ */
+  ROPE: {
+    code: 3,
+    name: "ロープ",
+  },
+  /** クラブ　*/
+  CLUB: {
+    code: 4,
+    name: "クラブ",
+  },
+} as const;
+
+export type Apparatus = (typeof APPARATUS)[keyof typeof APPARATUS];
+export const Apparatuses = Object.values(APPARATUS);
