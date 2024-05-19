fe82b9f49debfca9577144fc082e1a39707eeb78
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-05-19 21:10:20 +0900

    PointAの合計を算出するメソッドを簡略化

--- src/view/page/judge/_service/culculator.ts
+++ src/view/page/judge/_service/culculator.ts
@@ -5,55 +5,13 @@ import type { ExecutionDeduct } from "../_model/execution-deduct";
  * @return Aの項目の減点の合計
  */
 export function getAmountOfPointA(data: ExecutionDeduct): number {
-  const beautifulPoseValue = data.pointA.beautifulPose
-    ? data.pointA.beautifulPose.option.value
-    : 0;
-  const flexibilityValue = data.pointA.flexibility
-    ? data.pointA.flexibility.option.value
-    : 0;
-  const naturalMovementValue = data.pointA.naturalMovement
-    ? data.pointA.naturalMovement.option.value
-    : 0;
-  const bendingWeightValue = data.pointA.bendingWeight
-    ? data.pointA.bendingWeight.option.value
-    : 0;
-  const jumpingHeightValue = data.pointA.jumpingHeight
-    ? data.pointA.jumpingHeight.option.value
-    : 0;
-  const bodyControlValue = data.pointA.bodyControl
-    ? data.pointA.bodyControl.option.value
-    : 0;
-  const heelRaiseValue = data.pointA.heelRaise
-    ? data.pointA.heelRaise.option.value
-    : 0;
-  const weaknessAndStrengthValue = data.pointA.weaknessAndStrength
-    ? data.pointA.weaknessAndStrength.option.value
-    : 0;
-  const connectMovementValue = data.pointA.connectMovement
-    ? data.pointA.connectMovement.option.value
-    : 0;
-  const apparatusControlValue = data.pointA.apparatusControl
-    ? data.pointA.apparatusControl.option.value
-    : 0;
-  const musicImageValue = data.pointA.musicImage
-    ? data.pointA.musicImage.option.value
-    : 0;
+  const total = Object.values(data.pointA).reduce((sum, current) => {
+    const value = current?.option?.value || 0;
+    return sum + value * 100; // 小数点の誤差をなくすため整数で計算
+  }, 0);
 
   /** @note 小数点の誤差をなくすため整数で計算してから元に戻している */
-  return (
-    (beautifulPoseValue * 100 +
-      flexibilityValue * 100 +
-      naturalMovementValue * 100 +
-      bendingWeightValue * 100 +
-      jumpingHeightValue * 100 +
-      bodyControlValue * 100 +
-      heelRaiseValue * 100 +
-      weaknessAndStrengthValue * 100 +
-      connectMovementValue * 100 +
-      apparatusControlValue * 100 +
-      musicImageValue * 100) /
-    100
-  );
+  return total / 100;
 }
 
 /** @note Bの減点項目の合計を返す。減点のMaxを超えた場合はMaxの値を返す。 */
@@ -70,6 +28,7 @@ export function getAmountOfPointB(data: ExecutionDeduct): number {
 
 export function getMaxPointB(data: ExecutionDeduct): number {
   const pointA = getAmountOfPointA(data);
+
   return 10 - pointA;
 }
 
