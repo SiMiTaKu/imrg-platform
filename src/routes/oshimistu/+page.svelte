d8411e3cb1028ce2243db7a1ec1da3771e991c34
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-07-16 18:25:07 +0900

    骨組み作成

---
+++ src/routes/oshimistu/+page.svelte
@@ -0,0 +1,11 @@
+<script context='module' lang='ts'>
+  import Page from "../../view/page/oshimistu/Page.svelte"
+</script>
+
+<svelte:head>
+  <title
+  >オシミツ！あなたの推しを見つけよう〜｜男子新体操国際化プロジェクト</title
+  >
+</svelte:head>
+
+<Page />
