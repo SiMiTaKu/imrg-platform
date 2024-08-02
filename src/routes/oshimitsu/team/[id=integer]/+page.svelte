bffa52a305370dc2663b590fa6015673ae1487f1
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-02 18:29:34 +0900

    ファイル参照によるエラーを解消

--- src/routes/oshimitsu/team/[id=integer]/+page.svelte
+++ src/routes/oshimitsu/team/[id=integer]/+page.svelte
@@ -1,5 +1,5 @@
 <script context='module' lang='ts'>
-  import Page from "../../../view/page/oshimitsu/search_result/Page.svelte"
+  import Page from "../../../../view/page/oshimitsu/team/Page.svelte"
 </script>
 
 <svelte:head>
