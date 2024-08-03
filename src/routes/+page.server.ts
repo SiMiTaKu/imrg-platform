997e669167f2b59e29aa7b47129f158e21ba6482
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-03 23:41:15 +0900

    ssrの設定をページ全体に適応させる対応

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,6 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 
-export const prerender = true
-
 import type { ViewValueLayout } from "@model/view-value-layout"
 import type { PageServerLoad } from "./$types"
 
