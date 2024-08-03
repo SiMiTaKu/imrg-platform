b03fc54a94a6c250ee2cf0f8c8b057bc4e6d050b
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-04 00:03:06 +0900

    全ページに適応させるためのグループ化対応

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,5 @@
+import type { LayoutServerLoad } from "./$types"
+
 /**
  * この設定でSSRを有効にしている。
  * SSRを有効にすると、ページ遷移時にサーバー側でページを生成するため、ページ遷移が遅くなる可能性がある。
@@ -6,3 +8,7 @@
  * @see https://kit.svelte.jp/docs/page-options#ssr
  */
 export const prerender = true
+
+export const load: LayoutServerLoad = () => {
+  return undefined
+}
