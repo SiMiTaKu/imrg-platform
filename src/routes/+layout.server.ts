b8b26bf0ed0d731464a91617fb6bfff2c41a796f
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-03 23:41:15 +0900

    ssrの設定をページ全体に適応させる対応

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,8 @@
+/**
+ * この設定でSSRを有効にしている。
+ * SSRを有効にすると、ページ遷移時にサーバー側でページを生成するため、ページ遷移が遅くなる可能性がある。
+ * この設定を行わないと+page.server.tsのload関数が呼ばれないため、サーバー側でのデータ取得ができない。
+ * そのため、サーバー側でのデータ取得が必要な場合は、この設定を行う必要がある。
+ * @see https://kit.svelte.jp/docs/page-options#ssr
+ */
+export const prerender = true
