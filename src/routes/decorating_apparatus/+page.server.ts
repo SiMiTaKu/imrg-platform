a56ebc88b9157e4c25c924e933458a31e62121bd
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-03 23:33:53 +0900

    Revert "PC・SPの出し訳部分を削除"
    This reverts commit 3d16abeaf34e275450a6007b03a68a247f7d4e7d.

--- src/routes/decorating_apparatus/+page.server.ts
+++ src/routes/decorating_apparatus/+page.server.ts
@@ -1,21 +1,23 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 
-import type { ViewValueLayout } from "@model/view-value-layout"
+import { ViewValueLayout } from "@model/view-value-layout"
 import type { PageServerLoad } from "./$types"
 
 type OutputData = {
+  isMobile: boolean;
   origin: string;
   layout: ViewValueLayout;
 };
 
-export const load: PageServerLoad<OutputData> = async ({ url }) => {
+export const load: PageServerLoad<OutputData> = async ({ url, request }) => {
   const title =
     "手具装飾 | 男子新体操国際化プロジェクト ~日本の文化を世界のスポーツへ~"
   const description =
     "男子新体操の曲について知ることができるページです。曲を使って演技をする男子新体操は、音楽との相性が重要です。ルール変更により歌詞の有無が問われなくなったため、そのジャンルは多岐に渡るようになりました。"
 
   return {
+    isMobile: ViewValueLayout.isMobile(request.headers),
     origin: url.origin,
     layout: {
       title,
--- src/routes/rules/+page.server.ts
+++ src/routes/rules/+page.server.ts
@@ -1,21 +1,23 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 
-import type { ViewValueLayout } from "@model/view-value-layout"
+import { ViewValueLayout } from "@model/view-value-layout"
 import type { PageServerLoad } from "./$types"
 
 type OutputData = {
+  isMobile: boolean;
   origin: string;
   layout: ViewValueLayout;
 };
 
-export const load: PageServerLoad<OutputData> = async ({ url }) => {
+export const load: PageServerLoad<OutputData> = async ({ url, request }) => {
   const title =
     "ルールブックページ | 男子新体操国際化プロジェクト ~日本の文化を世界のスポーツへ~"
   const description =
     "男子新体操のルールブックページです。細かいルールについて調べることが可能です。わからないことやより深いルールを知りたい方はこのページを閲覧してください。"
 
   return {
+    isMobile: ViewValueLayout.isMobile(request.headers),
     origin: url.origin,
     layout: {
       title,
