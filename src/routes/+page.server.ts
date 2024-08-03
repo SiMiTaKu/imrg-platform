d38c4632107741e12a8d8692702eec2898dbe475
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-03 23:33:04 +0900

    revert

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,20 +3,22 @@
 
 export const prerender = true
 
-import type { ViewValueLayout } from "@model/view-value-layout"
+import { ViewValueLayout } from "@model/view-value-layout"
 import type { PageServerLoad } from "./$types"
 
 type OutputData = {
+  isMobile: boolean;
   origin: string;
   layout: ViewValueLayout;
 };
 
-export const load: PageServerLoad<OutputData> = async ({ url }) => {
+export const load: PageServerLoad<OutputData> = async ({ url, request }) => {
   const title = "男子新体操国際化プロジェクト ~日本の文化を世界のスポーツへ~"
   const description =
     "男子新体操国際化プロジェクトのトップページです。男子新体操は日本発祥のスポーツで70年以上の歴史があるスポーツです。ただ日本で進化し続けてきたため海外での競技者が少なくオリンピックスポーツになっていない現状です。この魅力ある日本の文化スポーツを世界のスポーツ（オリンピックスポーツ）にするために活動しております。"
 
   return {
+    isMobile: ViewValueLayout.isMobile(request.headers),
     origin: url.origin,
     layout: {
       title,
