88fa43fcada809afabcd7e6d44d555fe9f15475f
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-04 20:26:41 +0900

    修正

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,30 +6,26 @@ import type { PageServerLoad } from "./$types"
 
 type OutputData = {
   isMobile: boolean;
-  origin: string;
   layout: ViewValueLayout;
-  url: string;
 };
 
-export const load: PageServerLoad<OutputData> = async ({ url, locals }) => {
+export const load: PageServerLoad<OutputData> = async ({ locals }) => {
   const title = "男子新体操国際化プロジェクト ~日本の文化を世界のスポーツへ~"
   const description =
     "男子新体操国際化プロジェクトのトップページです。男子新体操は日本発祥のスポーツで70年以上の歴史があるスポーツです。ただ日本で進化し続けてきたため海外での競技者が少なくオリンピックスポーツになっていない現状です。この魅力ある日本の文化スポーツを世界のスポーツ（オリンピックスポーツ）にするために活動しております。"
 
   return {
     isMobile: locals.isMobile,
-    origin: url.origin,
-    url: JSON.stringify(url),
     layout: {
       title,
       description,
       noindex: false,
       nofollow: false,
-      canonical: url.href,
+      canonical: true,
+      path: "/",
       ogp: {
         title,
         description,
-        url: url.href,
         type: "website",
       },
     },
