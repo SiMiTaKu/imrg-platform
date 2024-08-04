c5875b205b40f27019aa2b439245c2e36727d021
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-08-04 10:04:07 +0900

    ログ埋め込み

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -11,25 +11,25 @@ type OutputData = {
   url: string;
 };
 
-export const load: PageServerLoad<OutputData> = async ({ url, locals }) => {
+export const load: PageServerLoad<OutputData> = async ({ locals, request }) => {
   const title = "男子新体操国際化プロジェクト ~日本の文化を世界のスポーツへ~"
   const description =
     "男子新体操国際化プロジェクトのトップページです。男子新体操は日本発祥のスポーツで70年以上の歴史があるスポーツです。ただ日本で進化し続けてきたため海外での競技者が少なくオリンピックスポーツになっていない現状です。この魅力ある日本の文化スポーツを世界のスポーツ（オリンピックスポーツ）にするために活動しております。"
 
   return {
     isMobile: locals.isMobile,
-    origin: url.origin,
-    url: JSON.stringify(url),
+    origin: request.url,
+    url: request.url,
     layout: {
       title,
       description,
       noindex: false,
       nofollow: false,
-      canonical: url.href,
+      canonical: request.url,
       ogp: {
         title,
         description,
-        url: url.href,
+        url: request.url,
         type: "website",
       },
     },
