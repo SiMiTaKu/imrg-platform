b758745784554c23db2aa240bf6747a2fc0c1a40
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-09-22 12:10:06 +0900

    Imageモデルにタイプを追加

--- src/amplify/data/models/team-image.ts
+++ src/amplify/data/models/team-image.ts
@@ -1,10 +1,12 @@
 import { a } from "@aws-amplify/backend"
+import { Image } from "$shared/constants"
 
 export const TeamImage = a
   .model({
     id: a.integer().required(),
     teamId: a.integer().required(),
     order: a.integer().required(),
+    imageType: a.enum(Image.Types),
   })
   .identifier([ "id" ])
   .authorization((allow) => [ allow.guest().to([ "read" ]) ])
--- src/amplify/storage/resource.ts
+++ src/amplify/storage/resource.ts
@@ -1,3 +1,10 @@
 import { defineStorage } from "@aws-amplify/backend"
 
-export const storage = defineStorage({ name: "imrg" })
+export const storage = defineStorage({
+  name: "imrg",
+  access: (allow) => ({
+    "1/*": [
+      allow.guest.to([ "read", "write" ]), allow.entity("identity").to([ "read", "write", "delete" ]),
+    ],
+  }),
+})
