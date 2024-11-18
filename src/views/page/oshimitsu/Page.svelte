f100b1dfd5d83b225f3fd7be2ff330dfa8236702
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-11-18 23:03:43 +0900

    不要コード削除

--- src/views/page/oshimitsu/Page.svelte
+++ src/views/page/oshimitsu/Page.svelte
@@ -1,28 +1,8 @@
 <script lang='ts'>
-  import type { Schema } from "$amplify/data/resource"
-  import { generateClient } from "aws-amplify/data"
-  import { onMount } from "svelte"
-
-  const client = generateClient<Schema>()
-
-  let players
-
-  onMount(async () => {
-    players = await client.models.Player.list()
-  })
 </script>
 
 <div class='padding'>
-  おしみつ
-  {#if players}
-    <ul>
-      {#each players.data as player (player.id)}
-        <li>{player.name}</li>
-      {/each}
-    </ul>
-  {:else}
-    <p>loading...</p>
-  {/if}
+  <h1>推しミツ！</h1>
 </div>
 
 <style lang='scss'>
