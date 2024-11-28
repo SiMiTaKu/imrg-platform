737d8c3fe098b3d55b535fb206f04dcbc0205fff
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-11-29 00:09:33 +0900

    コメントアウト

--- src/views/page/oshimitsu/util/index.ts
+++ src/views/page/oshimitsu/util/index.ts
@@ -1,2 +1,2 @@
-export * from "./player"
-export * from "./team"
+// export * from "./player"
+// export * from "./team"
--- src/views/page/oshimitsu/util/team.ts
+++ src/views/page/oshimitsu/util/team.ts
@@ -1,9 +1,9 @@
-import type { TeamResource } from "../models"
-import { TEAMS } from "../data"
-
-export namespace Team {
-  /** チームを取得する */
-  export const findById = (id: number): TeamResource | undefined => {
-    return TEAMS.find((team) => team.id === id)
-  }
-}
+// import type { TeamResource } from "../models"
+// import { TEAMS } from "../data"
+//
+// export namespace Team {
+//   /** チームを取得する */
+//   export const findById = (id: number): TeamResource | undefined => {
+//     return TEAMS.find((team) => team.id === id)
+//   }
+// }
