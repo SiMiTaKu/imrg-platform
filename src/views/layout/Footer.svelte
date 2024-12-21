f0e37c9e4324544b0d107f00423d03475a4fb033
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-12-21 22:42:27 +0900

    Footer修正

--- src/views/layout/Footer.svelte
+++ src/views/layout/Footer.svelte
@@ -1,33 +1,191 @@
+<script context='module' lang='ts'>
+  import ImageAssets from "$views/atomic/image/ImageAssets.svelte"
+  import type { SrcMeta } from "$views/atomic/image/ImageAssets.svelte"
+  import YoutubeIcon from "$views/layout/_images/youtube-icon.png?w=256;512&format=webp&as=meta"
+  import InstagramIcon from "$views/layout/_images/instagram-icon.png?w=256;512&format=webp&as=meta"
+  import XIcon from "$views/layout/_images/x-icon.png?w=256;512&format=webp&as=meta"
+  import TiktokIcon from "$views/layout/_images/tiktok-icon.png?w=256;512&format=webp&as=meta"
+
+  type Sns = {
+    name: string;
+    href: string;
+    icon: {
+      srcMeta: SrcMeta[];
+      alt: string;
+      size: {
+        pc: {
+          width: number;
+          height: number;
+        };
+        sp: {
+          width: number;
+          height: number;
+        };
+      };
+    };
+  };
+
+  export const SNS: Sns[] = [
+    {
+      name: "YouTube",
+      href: "https://www.youtube.com/channel/UCK-OUvWo6IQz2W6Z-fH27BQ?sub_confirmation=1",
+      icon: {
+        srcMeta: YoutubeIcon,
+        alt: "YouTubeアイコン",
+        size: {
+          pc: {
+            width: 80,
+            height: 60,
+          },
+          sp: {
+            width: 40,
+            height: 30,
+          },
+        },
+      },
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/takumi.rg/",
+      icon: {
+        srcMeta: InstagramIcon,
+        alt: "Instagramアイコン",
+        size: {
+          pc: {
+            width: 72,
+            height: 72,
+          },
+          sp: {
+            width: 36,
+            height: 36,
+          },
+        },
+      },
+    },
+    {
+      name: "X(Twitter)",
+      href: "https://twitter.com/TakumiShimizu43",
+      icon: {
+        srcMeta: XIcon,
+        alt: "X（Twitter）アイコン",
+        size: {
+          pc: {
+            width: 60,
+            height: 60,
+          },
+          sp: {
+            width: 30,
+            height: 30,
+          },
+        },
+      },
+    },
+    {
+      name: "TikTok",
+      href: "https://www.tiktok.com/@takumishimizu43?lang=ja-JP",
+      icon: {
+        srcMeta: TiktokIcon,
+        alt: "TikTokアイコン",
+        size: {
+          pc: {
+            width: 72,
+            height: 72,
+          },
+          sp: {
+            width: 36,
+            height: 36,
+          },
+        },
+      },
+    },
+  ]
+</script>
+
 <script lang='ts'>
   import { pageData } from "../atomic/device-store/store"
 </script>
 
-<footer
-  class='footer'
+<section
+  class='contents-footer'
   class:pc={!$pageData.isMobile}
   class:sp={$pageData.isMobile}
 >
-  <div class='content'>
-    ©︎ Internationalize Men's Rhythmic Gymnastics Project 2020-2023
+  <div class='container'>
+    <div class='sns'>
+      {#each SNS as sns, index (index)}
+        <a
+          class='item'
+          href={sns.href}
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <div class='icon'>
+            <ImageAssets
+              width={$pageData.isMobile
+                ? sns.icon.size.sp.width
+                : sns.icon.size.pc.width}
+              height={$pageData.isMobile
+                ? sns.icon.size.sp.height
+                : sns.icon.size.pc.height}
+              alt={sns.icon.alt}
+              lazy={true}
+              srcMeta={sns.icon.srcMeta}
+              objectFit='cover'
+            />
+          </div>
+          {sns.name}
+        </a>
+      {/each}
+    </div>
   </div>
-</footer>
+</section>
 
 <style lang='scss'>
   .pc {
-    --font-size: 12px;
+    --width: 1024px;
+    --sns-gap: 16px;
+    --item-font-size: 16px;
+    --icon-min-height: 72px;
   }
 
   .sp {
-    --font-size: 10px;
+    --width: 90%;
+    --sns-gap: 8px;
+    --item-font-size: 14px;
+    --icon-min-height: 36px;
   }
 
-  .footer {
-    background: #666666;
+  .contents-footer {
+    padding: 40px 0;
+    background-color: #eeeeee;
   }
 
-  .content {
-    color: white;
-    font-size: var(--font-size);
-    text-align: center;
+  .container {
+    width: var(--width);
+    margin: 0 auto;
+  }
+
+  .sns {
+    display: flex;
+    gap: var(--sns-gap);
+    width: 100%;
+  }
+
+  .item {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 8px;
+    width: 100%;
+    text-decoration: none;
+    font-size: var(--item-font-size);
+    font-weight: bold;
+    color: #777777;
+  }
+
+  .icon {
+    display: grid;
+    place-items: center;
+    min-height: var(--icon-min-height);
   }
 </style>
