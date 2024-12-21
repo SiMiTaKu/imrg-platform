92a52d2cd06eeb9aade27582589bd831cf0da789
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-12-21 21:13:24 +0900

    ヘッダー修正

--- src/views/layout/Header.svelte
+++ src/views/layout/Header.svelte
@@ -1,102 +1,80 @@
 <script context='module' lang='ts'>
   import ImageAssets from "../atomic/image/ImageAssets.svelte"
-  import MainImage from "$images/common/imrg-logo.jpg?w=681;1363&format=webp;png;jpg&as=meta"
-
-  const LINKS = [
-    { href: "/", text: "トップページ" },
-    { href: "/decorating_apparatus", text: "手具装飾を依頼する" },
-    { href: "/background_music", text: "曲編集を依頼する" },
-    { href: "/judge", text: "審判を体験する" },
-    { href: "/rules", text: "ルールを知る" },
-  ]
+  import MainImage from "./_images/imrg-logo.jpg?w=650;1300&format=webp&as=meta"
+  import Hamburger from "$views/layout/_components/Hamburger.svelte"
 </script>
 
 <script lang='ts'>
   import { pageData } from "../atomic/device-store/store"
-  import { slide } from "svelte/transition"
 
-  let show = false
+  let isHamburgerOpen = false
 </script>
 
-<div class:pc={!$pageData.isMobile} class:sp={$pageData.isMobile}>
-  <header class='header-main'>
-    <div class='content'>
-      <a
-        class='header-link'
-        href='/'
-        on:click={() => (show ? (show = !show) : undefined)}
-      >
-        <div class='image'>
-          <ImageAssets
-            width={80}
-            height={80}
-            alt='男子新体操国際化プロジェクトロゴ'
-            lazy={false}
-            srcMeta={MainImage}
-            objectFit='cover'
-          />
-        </div>
-        <div class='title'>
-          <span class='main'>男子新体操国際化プロジェクト</span>
-          <span class='sub'>Internationalize Men's Rhythmic Gymnastics</span>
-        </div>
-      </a>
-      <div class='hamburger'>
-        <button
-          class='hamburger-button'
-          type='button'
-          on:click={() => (show = !show)}
-        >
-          <div class='hamburger-icon' class:open={show} />
-          <span class='hamburger-text'>{show ? "close" : "open"}</span>
-        </button>
-        {#if show}
-          <div class='hamburger-wrapper' transition:slide={{ duration: 500 }}>
-            <ul class='links'>
-              {#each LINKS as { href, text }, index (index)}
-                <li>
-                  <a class='link'
-                     {href}
-                     on:click={() => (show = !show)}
-                  >{text}</a
-                  >
-                </li>
-              {/each}
-            </ul>
-          </div>
-        {/if}
+<header
+  class='header-main'
+  class:pc={!$pageData.isMobile}
+  class:sp={$pageData.isMobile}
+>
+  <div class='content'>
+    <a
+      class='header-link'
+      href='/'
+      on:click={() =>
+        isHamburgerOpen ? (isHamburgerOpen = !isHamburgerOpen) : undefined}
+    >
+      <div class='image'>
+        <ImageAssets
+          width={$pageData.isMobile ? 80 : 110}
+          height={$pageData.isMobile ? 56 : 72}
+          alt='男子新体操国際化プロジェクトのロゴ'
+          lazy={false}
+          srcMeta={MainImage}
+          objectFit='cover'
+        />
       </div>
-    </div>
-  </header>
-</div>
+    </a>
+    <button
+      class='hamburger-button'
+      type='button'
+      on:click={() => (isHamburgerOpen = !isHamburgerOpen)}
+    >
+      <div class='hamburger-icon' class:open={isHamburgerOpen} />
+      <span class='hamburger-text'>{isHamburgerOpen ? "close" : "open"}</span>
+    </button>
+    <Hamburger
+      open={isHamburgerOpen}
+      on:close={() => (isHamburgerOpen = !isHamburgerOpen)}
+    />
+  </div>
+</header>
 
 <style lang='scss'>
   .pc {
     --height: 80px;
-    --image-size: 128px;
     --content-width: 1024px;
-    --content-grid-template-columns: 1fr auto 40px;
+    --header-link-padding: 0;
+    --image-padding: #{$space-size-4} 0;
+
     --header-link-grid-column: 1 / 3;
+    --humbarger-button-padding: 0;
     --humbarger-grid-column: 2 / 4;
     --hamburger-text-font-size: 20px;
-    --title-margin-top: 8px;
-    --main-font-size: 30px;
-    --sub-font-size: 12px;
+    --hamburger-wrapper-width: 400px;
     --button-icon-size: 32px;
     --button-icon-position-fix: -6px;
   }
 
   .sp {
-    --height: 64px;
-    --image-size: 102px;
+    --height: 60px;
     --content-width: 100%;
-    --content-grid-template-columns: 8px 1fr auto 48px;
+    --header-link-padding: 0 0 0 #{$space-size-8};
+    --image-padding: #{$space-size-2} 0;
+
     --header-link-grid-column: 2 / 4;
+    --humbarger-button-padding: 0 8px 0 0;
     --humbarger-grid-column: 3 / 5;
     --hamburger-text-font-size: 16px;
-    --title-margin-top: 10px;
-    --main-font-size: 22px;
-    --sub-font-size: 11px;
+    --hamburger-wrapper-width: 100vw;
     --button-icon-size: 24px;
     --button-icon-position-fix: -5px;
   }
@@ -104,10 +82,11 @@
   .header-main {
     position: fixed;
     display: grid;
-    align-items: center;
+    place-items: center;
     top: 0;
     width: 100vw;
     height: var(--height);
+    background: white;
     z-index: 1000;
 
     &:before {
@@ -116,103 +95,83 @@
       top: 0;
       width: 100vw;
       height: var(--height);
-      background: white;
-      box-shadow: 0 0 24px rgba(0, 0, 0, 0.3);
-      opacity: 0.8;
+      background: transparent;
+      box-shadow: $black-box-shadow;
+      pointer-events: none;
+      user-select: none;
     }
   }
 
   .content {
     position: relative;
     display: grid;
-    grid-template-columns: var(--content-grid-template-columns);
-    grid-template-rows: auto auto;
+    grid-template-columns: 1fr auto;
+    place-items: center;
+    gap: 8px;
     width: var(--content-width);
     height: 100%;
-    margin: 0 auto;
+    box-sizing: border-box;
   }
 
   .header-link {
-    grid-column: var(--header-link-grid-column);
-    grid-row: 1 / 2;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 8px auto 1fr;
+    grid-template-areas: "logo ." "logo sub-title" "logo title";
+    gap: 0 8px;
+    align-items: center;
+    justify-items: left;
+    padding: var(--header-link-padding);
+    width: 100%;
+    height: 100%;
     text-decoration: none;
-    overflow: hidden;
+    box-sizing: border-box;
   }
 
   .image {
-    position: absolute;
-    top: 0;
-    width: var(--image-size);
-    height: var(--height);
-    opacity: 0.1;
+    padding: var(--image-padding);
+    box-sizing: border-box;
+    grid-area: logo;
     pointer-events: none;
-  }
-
-  .title {
-    display: grid;
-    margin-top: var(--title-margin-top);
-    font-weight: bold;
-    color: #555;
-    pointer-events: none;
-  }
-
-  .main {
-    display: block;
-    font-size: var(--main-font-size);
-    text-shadow: 0 0 4px rgba(50, 150, 255, 0.5);
-  }
-
-  .sub {
-    display: block;
-    font-size: var(--sub-font-size);
-    text-shadow: 0 0 4px rgba(50, 150, 255, 0.5);
   }
 
   //#region hamburger
-  .hamburger {
-    display: grid;
-    justify-items: right;
-    grid-column: var(--humbarger-grid-column);
-    grid-row: 1 / 3;
-  }
-
   .hamburger-button {
     display: grid;
-    justify-items: center;
-    align-items: center;
-    grid-template-rows: var(--button-icon-size) 1fr 1fr 1fr 1fr 1fr;
-    gap: 4px;
-    width: 48px;
+    place-items: center;
+    grid-template-rows: 1fr var(--button-icon-size) 1fr 1fr;
+    width: var(--height);
     height: var(--height);
-    transition: 0.3s;
+    padding: var(--humbarger-button-padding);
+    transition: 0.25s;
     border: none;
+    box-sizing: content-box;
     background: transparent;
 
     &:active {
-      background-color: #f0f0f0aa;
+      background: map.get($sky-blue, background);
     }
 
     &:hover {
-      background-color: #f0f0f0aa;
+      background: map.get($sky-blue, background);
     }
   }
 
   .hamburger-icon {
     display: grid;
     position: relative;
-    grid-row: 1 / 5;
-    grid-column: 1 / 2;
+    grid-row: 2;
+    grid-column: 1;
     width: var(--button-icon-size);
     height: 3px;
     border-radius: 2em;
     background: #555;
     transition: 0.25s;
 
-    &:before {
-      content: "";
+    &:before,
+    &:after {
       position: absolute;
-      top: calc(var(--button-icon-size) / -4);
-      right: 0;
+      content: "";
       width: var(--button-icon-size);
       height: 3px;
       background: #555;
@@ -220,16 +179,14 @@
       transition: 0.25s;
     }
 
+    &:before {
+      top: calc(var(--button-icon-size) / -4);
+      right: 0;
+    }
+
     &:after {
-      content: "";
-      position: absolute;
       top: calc(var(--button-icon-size) / 4);
       left: 0;
-      width: var(--button-icon-size);
-      height: 3px;
-      background: #555;
-      border-radius: 2em;
-      transition: 0.25s;
     }
   }
 
@@ -249,44 +206,10 @@
   }
 
   .hamburger-text {
-    grid-row: 4 / 5;
-    grid-column: 1 / 2;
+    grid-row: 3;
+    grid-column: 1;
     color: #555;
     font-size: var(--hamburger-text-font-size);
-  }
-
-  .hamburger-wrapper {
-    display: grid;
-    overflow: hidden;
-    border-radius: 0 0 8px 8px;
-    background-color: #eee;
-    height: 240px;
-  }
-
-  .links {
-    display: grid;
-    height: min-content;
-    list-style: none;
-    font-size: 18px;
-    font-weight: bold;
-    padding: 8px 16px;
-  }
-
-  .links > li {
-    transition: 0.3s;
-
-    &:not(:last-child) {
-      border-bottom: 1px solid #999;
-    }
-
-    &:hover {
-      background-color: #f0f0f0;
-    }
-  }
-
-  .link {
-    display: grid;
-    padding: 8px 16px;
   }
   //#endregion
 </style>
---
+++ src/views/layout/_components/Hamburger.svelte
@@ -0,0 +1,89 @@
+<script context='module' lang='ts'>
+  import { createEventDispatcher } from "svelte"
+
+  const LINKS = [
+    { href: "/", text: "トップページ" },
+    { href: "/decorating_apparatus", text: "手具装飾を依頼する" },
+    { href: "/background_music", text: "曲編集を依頼する" },
+    { href: "/judge", text: "審判を体験する" },
+    { href: "/rules", text: "ルールを知る" },
+  ]
+</script>
+
+<script lang='ts'>
+  import { pageData } from "$views/atomic/device-store/store"
+
+  const dispatch = createEventDispatcher<{ close: void }>()
+
+  export let open: boolean
+</script>
+
+<div
+  class='wrapper'
+  class:pc={!$pageData.isMobile}
+  class:sp={$pageData.isMobile}
+  class:open
+>
+  <ul class='links'>
+    {#each LINKS as { href, text }, index (index)}
+      <li>
+        <a class='link'
+           {href}
+           on:click={() => dispatch("close")}>{text}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style lang='scss'>
+  .pc {
+    --wrapper-width: 400px;
+  }
+
+  .sp {
+    --wrapper-width: 100vw;
+  }
+
+  .wrapper {
+    position: absolute;
+    top: var(--height);
+    right: 0;
+    display: grid;
+    overflow: hidden;
+    border-radius: 0 0 $border-radius-8 $border-radius-8;
+    background-color: map.get($sky-blue, background);
+    height: 0;
+    width: var(--wrapper-width);
+    transition: 0.3s;
+  }
+
+  .open {
+    height: 240px;
+  }
+
+  .links {
+    display: grid;
+    height: min-content;
+    list-style: none;
+    font-size: 18px;
+    font-weight: bold;
+    padding: $space-size-8 $space-size-16;
+  }
+
+  .links > li {
+    transition: 0.3s;
+
+    &:not(:last-child) {
+      border-bottom: $border-size-1 solid map.get($sky-blue, border);
+    }
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  .link {
+    display: grid;
+    padding: $space-size-8 $space-size-16;
+  }
+</style>
