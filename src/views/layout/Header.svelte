2911183932bc61f3a74927ba33ebc23c88b7c407
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2024-12-21 22:51:03 +0900

    ハンバーガーをコンポーネント分裂

--- src/views/layout/Header.svelte
+++ src/views/layout/Header.svelte
@@ -6,6 +6,7 @@
 
 <script lang='ts'>
   import { pageData } from "../atomic/device-store/store"
+  import HamburgerSwitchButton from "$views/layout/_components/HamburgerSwitchButton.svelte"
 
   let isHamburgerOpen = false
 </script>
@@ -24,8 +25,8 @@
     >
       <div class='image'>
         <ImageAssets
-          width={$pageData.isMobile ? 80 : 110}
-          height={$pageData.isMobile ? 56 : 72}
+          width={$pageData.isMobile ? 80 : 100}
+          height={$pageData.isMobile ? 56 : 64}
           alt='男子新体操国際化プロジェクトのロゴ'
           lazy={false}
           srcMeta={MainImage}
@@ -33,14 +34,10 @@
         />
       </div>
     </a>
-    <button
-      class='hamburger-button'
-      type='button'
+    <HamburgerSwitchButton
+      isOpen={isHamburgerOpen}
       on:click={() => (isHamburgerOpen = !isHamburgerOpen)}
-    >
-      <div class='hamburger-icon' class:open={isHamburgerOpen} />
-      <span class='hamburger-text'>{isHamburgerOpen ? "close" : "open"}</span>
-    </button>
+    />
     <Hamburger
       open={isHamburgerOpen}
       on:close={() => (isHamburgerOpen = !isHamburgerOpen)}
@@ -53,30 +50,14 @@
     --height: 80px;
     --content-width: 1024px;
     --header-link-padding: 0;
-    --image-padding: #{$space-size-4} 0;
-
-    --header-link-grid-column: 1 / 3;
-    --humbarger-button-padding: 0;
-    --humbarger-grid-column: 2 / 4;
-    --hamburger-text-font-size: 20px;
-    --hamburger-wrapper-width: 400px;
-    --button-icon-size: 32px;
-    --button-icon-position-fix: -6px;
+    --image-padding: #{$space-size-8} 0;
   }
 
   .sp {
-    --height: 60px;
+    --height: 64px;
     --content-width: 100%;
     --header-link-padding: 0 0 0 #{$space-size-8};
-    --image-padding: #{$space-size-2} 0;
-
-    --header-link-grid-column: 2 / 4;
-    --humbarger-button-padding: 0 8px 0 0;
-    --humbarger-grid-column: 3 / 5;
-    --hamburger-text-font-size: 16px;
-    --hamburger-wrapper-width: 100vw;
-    --button-icon-size: 24px;
-    --button-icon-position-fix: -5px;
+    --image-padding: #{$space-size-4} 0;
   }
 
   .header-main {
@@ -134,82 +115,4 @@
     grid-area: logo;
     pointer-events: none;
   }
-
-  //#region hamburger
-  .hamburger-button {
-    display: grid;
-    place-items: center;
-    grid-template-rows: 1fr var(--button-icon-size) 1fr 1fr;
-    width: var(--height);
-    height: var(--height);
-    padding: var(--humbarger-button-padding);
-    transition: 0.25s;
-    border: none;
-    box-sizing: content-box;
-    background: transparent;
-
-    &:active {
-      background: map.get($sky-blue, background);
-    }
-
-    &:hover {
-      background: map.get($sky-blue, background);
-    }
-  }
-
-  .hamburger-icon {
-    display: grid;
-    position: relative;
-    grid-row: 2;
-    grid-column: 1;
-    width: var(--button-icon-size);
-    height: 3px;
-    border-radius: 2em;
-    background: #555;
-    transition: 0.25s;
-
-    &:before,
-    &:after {
-      position: absolute;
-      content: "";
-      width: var(--button-icon-size);
-      height: 3px;
-      background: #555;
-      border-radius: 2em;
-      transition: 0.25s;
-    }
-
-    &:before {
-      top: calc(var(--button-icon-size) / -4);
-      right: 0;
-    }
-
-    &:after {
-      top: calc(var(--button-icon-size) / 4);
-      left: 0;
-    }
-  }
-
-  .hamburger-icon.open {
-    transform: rotate(90deg);
-    height: 0;
-
-    &:before {
-      transform: rotate(45deg);
-      top: 50%;
-    }
-
-    &:after {
-      transform: rotate(135deg);
-      top: 50%;
-    }
-  }
-
-  .hamburger-text {
-    grid-row: 3;
-    grid-column: 1;
-    color: #555;
-    font-size: var(--hamburger-text-font-size);
-  }
-  //#endregion
 </style>
--- src/views/layout/_components/Hamburger.svelte
+++ src/views/layout/_components/Hamburger.svelte
@@ -7,6 +7,7 @@
     { href: "/background_music", text: "曲編集を依頼する" },
     { href: "/judge", text: "審判を体験する" },
     { href: "/rules", text: "ルールを知る" },
+    { href: "/oshimitsu", text: "推しミツ" },
   ]
 </script>
 
@@ -58,7 +59,7 @@
   }
 
   .open {
-    height: 240px;
+    height: 280px;
   }
 
   .links {
