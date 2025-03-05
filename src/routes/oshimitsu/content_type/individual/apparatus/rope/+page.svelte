6027822a2da490fba4cff5b3d0e72f0a9b086959
SiMiTaKu <34091968+SiMiTaKu@users.noreply.github.com>
2025-03-05 22:43:54 +0900

    手具絞り込みページ作成

---
+++ src/routes/oshimitsu/content_type/individual/apparatus/rope/+page.svelte
@@ -0,0 +1,14 @@
+<script context='module' lang='ts'>
+  import Page from "$views/page/oshimitsu/content_type/Page.svelte"
+  import Layout from "$views/layout/Layout.svelte"
+</script>
+
+<script lang='ts'>
+  import { page } from "$app/stores"
+</script>
+
+<Layout layout={$page.data.layout}>
+  <Page slot='main'
+        videos={$page.data.videos}
+        criteria={$page.data.criteria} />
+</Layout>
